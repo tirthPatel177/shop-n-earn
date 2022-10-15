277511ae1969394850786880db9b7619b119a007
nipun3333 <nipunpatel3333@gmail.com>
2022-10-16 00:33:56 +0530

    landing alignment

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -8,9 +8,9 @@ const LandingPage = () => {
         <LandingHeader />
       </div>
 
-      <div>
-        <div>
-          <div className='mt-10 px-16'>
+      <div className="flex items-center justify-center">
+        <div style={{ maxWidth: "1400px" }}>
+          <div className='mt-10 px-16 flex items-center justify-center'>
             <div className='flex flex-col md:flex-row items-center'>
               <div className="w-full md:w-1/2">
                 <div className='text-center md:text-left'>
@@ -18,7 +18,7 @@ const LandingPage = () => {
                   <p className="mt-4 text-center text-sm md:text-base md:text-left">Reward your customers, increase sales, and drive repeat business… without lifting a finger!</p>
                   <div className="flex items-center justify-center md:flex md:items-center md:justify-start">
                     <a href="https://app.finplan.io/auth/login/">
-                      <p className='text-text1 font-medium text-sm md:text-base w-32 text-center rounded-md bg-primary px-3 py-2 mt-3'>
+                      <p className='text-text1 font-medium text-sm md:text-base text-center rounded-md bg-primary px-3 py-2 mt-3'>
                         Get started
                       </p>
                     </a>
@@ -26,7 +26,7 @@ const LandingPage = () => {
                 </div>
               </div>
               <div className='w-full md:w-1/2 flex items-center justify-center'>
-                <div className='w-96'>
+                <div className=''>
                   image
                 </div>
               </div>
