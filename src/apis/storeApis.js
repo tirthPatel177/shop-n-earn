4cbfffa179138db4240c5c509f715ec6be923aa7
Tirth Patel <177tpatel@gmail.com>
2022-10-16 12:31:12 +0530

    :hammer: Added auth header service

--- src/apis/storeApis.js
+++ src/apis/storeApis.js
@@ -1,9 +1,14 @@
 import axios from "axios";
+import authHeader from "../config/authHeader";
 import config from "./../config";
 
 export const getAllStore = async () => {
+  let header = await authHeader();
+
   const result = await axios
-    .get(config.apiEndPoint + "api/store/get")
+    .get(config.apiEndPoint + "api/store/get", {
+      headers: header,
+    })
     .then((res) => {
       console.log("success api called", res);
       return res.data;
